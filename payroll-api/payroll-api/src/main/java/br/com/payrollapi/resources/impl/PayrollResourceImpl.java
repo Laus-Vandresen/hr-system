3e81c68ff6c55f08c2516baebc4f9a07b7627f08
Laus Vandresen <lausvan1205@gmail.com>
2024-09-28 09:31:54 -0300

    Docker compose builded

---
+++ payroll-api/payroll-api/src/main/java/br/com/payrollapi/resources/impl/PayrollResourceImpl.java
@@ -0,0 +1,24 @@
+package br.com.payrollapi.resources.impl;
+
+
+import br.com.payrollapi.domain.Payroll;
+import br.com.payrollapi.services.PayrollService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/payments")
+public class PayrollResourceImpl {
+
+    private final PayrollService payrollService;
+
+    @RequestMapping("/{workerId}")
+    public ResponseEntity<Payroll> getPayment(@PathVariable Long workerId, @RequestBody Payroll payment) {
+        return ResponseEntity.ok(payrollService.getPayment(workerId, payment));
+    }
+}
